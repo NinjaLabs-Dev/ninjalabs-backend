f3894cada434663dc90bc7f090ba74bc1b12e6eb
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2021-01-16 19:20:58 +0000

    Fixing old support issue

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -37,7 +37,7 @@ class DocumentController extends Controller
     public function redirectToNew($slug) {
         $slug = explode(".", $slug);
         $slug = $slug[0];
-        return Redirect::route('image', $slug);
+        return Redirect::route('image');
     }
 
     public function store(Request $request) {
