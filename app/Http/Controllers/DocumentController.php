9765e800f3e75b6c8a7706206a244edbdd70fd4a
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2021-02-08 15:53:12 +0000

    Fixing issues

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -38,7 +38,7 @@ class DocumentController extends Controller
     public function redirectToNew($slug) {
         $slug = explode(".", $slug);
         $slug = $slug[0];
-        return DB::where('slug', $slug)->get()->toArray();
+        return Image::where('slug', $slug)->get()->toArray();
     }
 
     public function store(Request $request) {
