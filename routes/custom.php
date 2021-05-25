86962fb8f4e4b9058c88dd121d8528f21308c7e4
NinjaLabs-Dev <fraser@ninjalabs.co.uk>
2021-05-25 16:23:24 +0100

    Testing

--- routes/custom.php
+++ routes/custom.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Pages\DashboardController;
+use App\Http\Controllers\DocumentController;
 
 Route::get('/{slug}', [DocumentController::class, 'index'])->name('image');
