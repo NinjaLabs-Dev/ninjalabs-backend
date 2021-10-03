cbcdb2ebb0a6ad631d4eecb7bfea7a685094e1ed
NinjaLabs-Dev <fraser@ninjalabs.dev>
2021-10-03 01:36:49 +0100

    Added test

--- app/Http/Controllers/Pages/ServerStatsController.php
+++ app/Http/Controllers/Pages/ServerStatsController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 use App\Models\Server;
 use App\Models\ServerStat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class ServerStatsController extends Controller
 {
@@ -20,14 +21,16 @@ class ServerStatsController extends Controller
         $servers = Server::with(['repos'])->get();
 
         foreach ($servers as $server) {
-            $stat = ServerStat::where('server_id', $server->id)->orderBy('created_at', 'desc')->first();
+            Cache::remember('server-stats-overview-' . $server->id, 900, function() use($server) {
+                $stat = ServerStat::where('server_id', $server->id)->orderBy('created_at', 'desc')->get();
 
-            if ($stat->isEmpty()) {
-                $stat["ssh_connections"] = 0;
-                $stat["uptime"] = 0;
-            }
+                if ($stat->isEmpty()) {
+                    $stat["ssh_connections"] = 0;
+                    $stat["uptime"] = 0;
+                }
 
-            $server["stat"] = $stat;
+                $server["stat"] = $stat->first();
+            });
         }
 
         return view('pages.server-stats.index')->with('servers', $servers);
