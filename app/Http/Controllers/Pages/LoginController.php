50450b2ca7ebfb858369c473c8ef8d4e3217d3fc
NinjaLabs <fraser@ninjalabs.co.uk>
2023-01-07 18:16:51 +0000

    fix(Login): login failure loop

--- app/Http/Controllers/Pages/LoginController.php
+++ app/Http/Controllers/Pages/LoginController.php
@@ -14,7 +14,7 @@ class LoginController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['guest'])->except('login');
+        $this->middleware(['guest'])->except('logout');
         $this->middleware(['auth'])->only('logout');
     }
 
@@ -28,7 +28,7 @@ class LoginController extends Controller
             'password' => 'required'
         ]);
 
-        if($validator->errors()) {
+        if($validator->fails()) {
             return redirect()->back()->withErrors($validator->errors()->first());
         }
 
